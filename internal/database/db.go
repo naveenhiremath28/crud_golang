fd055077964cae22cef46a5ee10e0edd94d2da39
Naveen <152955809+naveenhiremath28@users.noreply.github.com>
2025-08-14 12:41:19 +0530

    added git ignore file

--- internal/database/db.go
+++ internal/database/db.go
@@ -5,16 +5,35 @@ import (
 	"fmt"
 	// "database/sql"
     // _ "github.com/lib/pq"
-
+	"os"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
+func getEnv(key, fallback string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return fallback
+}
+
 func Connect() error {
 	var err error
-	connStr := "host=localhost port=5431 user=postgres password=postgres dbname=go_test sslmode=disable"
+	if err := godotenv.Load(); err != nil {
+        log.Println("No .env file found, using system environment variables")
+    }
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		getEnv("DB_HOST", "localhost"),
+		getEnv("DB_PORT", "5432"),
+		getEnv("DB_USER", "postgres"),
+		getEnv("DB_PASSWORD", "password"),
+		getEnv("DB_NAME", "mydb"),
+	)
+
 	// DB, err = sql.Open("postgres", connStr)
 	DB, err = gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
